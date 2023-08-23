0a21928d96ee906fdf0ccefac25a31011f1ab585
LASwish05 <lorenz.alexander83@gmail.com>
2023-08-23 11:44:46 +0200

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+    return(
+        <header className="header">
+            <img src="/images/laswish_friendly_lion_baby_head_logo_drawn_in_black_and_white_f_7be656fd-08f5-4547-98d4-7511bddff952.png" className="header--image" />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project</h4>
+        </header>
+        
+    )
+}
